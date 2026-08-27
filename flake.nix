bd369b4ded5bc98677619f61101817637b607a2a
Harivansh Rathi <rathiharivansh@gmail.com>
2026-08-27 19:20:25 +0000

    fix(nix): fetch crates from static registry

--- flake.nix
+++ flake.nix
@@ -7,35 +7,52 @@
   };
 
   outputs =
-    { self, nixpkgs, flake-utils }:
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
         lib = pkgs.lib;
         cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+        crateFetchurl =
+          args:
+          let
+            crate = builtins.match "https://crates.io/api/v1/crates/([^/]+)/([^/]+)/download" args.url;
+          in
+          pkgs.fetchurl (
+            args
+            // lib.optionalAttrs (crate != null) {
+              url = "https://static.crates.io/crates/${builtins.elemAt crate 0}/${builtins.elemAt crate 1}/download";
+            }
+          );
+        cargoDeps = (pkgs.rustPlatform.importCargoLock.override { fetchurl = crateFetchurl; }) {
+          lockFile = ./Cargo.lock;
+        };
 
-        deskctl =
-          pkgs.rustPlatform.buildRustPackage {
-            pname = cargoToml.package.name;
-            version = cargoToml.package.version;
-            src = ./.;
-            cargoLock.lockFile = ./Cargo.lock;
-            nativeBuildInputs = [ pkgs.pkg-config ];
-            buildInputs = lib.optionals pkgs.stdenv.isLinux [
-              pkgs.libx11
-              pkgs.libxtst
-            ];
-            doCheck = false;
+        deskctl = pkgs.rustPlatform.buildRustPackage {
+          pname = cargoToml.package.name;
+          version = cargoToml.package.version;
+          src = ./.;
+          inherit cargoDeps;
+          nativeBuildInputs = [ pkgs.pkg-config ];
+          buildInputs = lib.optionals pkgs.stdenv.isLinux [
+            pkgs.libx11
+            pkgs.libxtst
+          ];
+          doCheck = false;
 
-            meta = with lib; {
-              description = cargoToml.package.description;
-              homepage = cargoToml.package.homepage;
-              license = licenses.mit;
-              mainProgram = "deskctl";
-              platforms = platforms.linux;
-            };
+          meta = with lib; {
+            description = cargoToml.package.description;
+            homepage = cargoToml.package.homepage;
+            license = licenses.mit;
+            mainProgram = "deskctl";
+            platforms = platforms.linux;
           };
+        };
       in
       {
         formatter = pkgs.nixfmt;
@@ -55,22 +72,21 @@
         };
 
         devShells.default = pkgs.mkShell {
-          packages =
-            [
-              pkgs.cargo
-              pkgs.clippy
-              pkgs.nodejs
-              pkgs.nixfmt
-              pkgs.pkg-config
-              pkgs.pnpm
-              pkgs.rustc
-              pkgs.rustfmt
-            ]
-            ++ lib.optionals pkgs.stdenv.isLinux [
-              pkgs.libx11
-              pkgs.libxtst
-              pkgs.xorg.xorgserver
-            ];
+          packages = [
+            pkgs.cargo
+            pkgs.clippy
+            pkgs.nodejs
+            pkgs.nixfmt
+            pkgs.pkg-config
+            pkgs.pnpm
+            pkgs.rustc
+            pkgs.rustfmt
+          ]
+          ++ lib.optionals pkgs.stdenv.isLinux [
+            pkgs.libx11
+            pkgs.libxtst
+            pkgs.xorg.xorgserver
+          ];
         };
       }
     );
